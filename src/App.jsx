3f8af5698c2ae4383290ce616f2feebc921188df
Mag1sstr <danilkarachev234@mail.ru>
2024-10-31 19:21:42 +0500

    add song,images

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
+      <div className="h-screen bg-black">
+        <div className="h-[90%] flex text-sky-300">wdwdwd</div>
       </div>
     </>
   );
