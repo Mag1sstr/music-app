339ce21e4a03ff0a441c5caefdfdeafc486897f5
Mag1sstr <danilkarachev234@mail.ru>
2024-11-01 17:21:44 +0500

    add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import "./App.css";
+import Player from "./components/Player";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <div className="h-screen bg-black">
-        <div className="h-[90%] flex text-sky-300">wdwdwd</div>
+        <div className="h-[90%] flex">
+          <Sidebar />
+        </div>
+        <Player />
       </div>
     </>
   );
