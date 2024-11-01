1f5bb2705f76fec4a7bea96d06a54f03a3e896a0
Mag1sstr <danilkarachev234@mail.ru>
2024-11-02 04:53:16 +0500

    add display component

---
+++ src/components/DisplayHome.jsx
@@ -0,0 +1,40 @@
+import Navbar from "./Navbar";
+import { albumsData, songsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import SongItem from "./SongItem";
+
+export default function DisplayHome() {
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">FeaturedCharts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              image={item.image}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
